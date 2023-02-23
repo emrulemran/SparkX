2c4f4457ff6cfed4d99b2464b4ab2191511e4564
emrulemran <emrulhasan@gmail.com>
2023-02-22 23:00:27 -0500

    3 example of rdd creation

--- src/main/scala/RDDCreation.scala
+++ src/main/scala/RDDCreation.scala
@@ -1,3 +1,4 @@
+import org.antlr.v4.runtime.atn.SemanticContext.AND
 import org.apache.spark.sql.SparkSession
 
 object RDDCreation {
@@ -7,11 +8,27 @@ object RDDCreation {
 
     val spark = SparkSession.builder().appName("a").master("local[*]").getOrCreate()
 
+    spark.sparkContext.setLogLevel("ERROR")
+
     // from a text file:
-    var path: String = "///TextData.txt"
-    var textRDD = spark.sparkContext.textFile("path")
+    var path: String = "data/TextData.txt"
+    var textRDD = spark.sparkContext.textFile(path)
     textRDD.foreach(println)
 
+    println("-------------------------------------------------------------------------")
+
+    // by parallelizing a collection
+    var cittList: List[String] = List("boston", "san jose", "miami", "oklahoma")
+
+    var cityRDD = spark.sparkContext.parallelize(cittList)
+    cityRDD.foreach(println)
+    println("-------------------------------------------------------------------------")
+
+    // by transforming an existing RDD:
+    var numList: List[Double] = List(12, 45, 67, 89, 45, 45, 76, 34, 3, 76, 34, 35, 576, 74, 345, 79, 87, 14, 85)
+    var outputRDD = numList.map(x => x/5)
+    outputRDD.foreach(println)
+    println("-------------------------------------------------------------------------")
 
 
   }
